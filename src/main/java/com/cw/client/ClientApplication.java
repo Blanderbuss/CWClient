02ac2fb28e67a7d3b1764c5fb12cc4351a9f6526
Blanderbuss <fedorakb@gmail.com>
2018-04-01 23:20:21 +0300

    Services for all for free

--- src/main/java/com/cw/client/ClientApplication.java
+++ src/main/java/com/cw/client/ClientApplication.java
@@ -2,6 +2,9 @@ package com.cw.client;
 
 import com.cw.appif.ServerServiceIF;
 import com.cw.exceptions.UserException;
+import com.cw.models.db.services.ArtefactServiceI;
+import com.cw.models.db.services.SetServiceI;
+import com.cw.models.db.services.UserServiceI;
 import com.cw.models.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,12 +19,36 @@ public class ClientApplication {
     //TODO add methods for creating fighters
 
 	@Bean
-    RmiProxyFactoryBean service() {
+    RmiProxyFactoryBean battleService() {
 		RmiProxyFactoryBean rmiProxyFactory = new RmiProxyFactoryBean();
             rmiProxyFactory.setServiceUrl("rmi://localhost:1099/ServerServiceIF");
 		rmiProxyFactory.setServiceInterface(ServerServiceIF.class);
 		return rmiProxyFactory;
 	}
+
+    @Bean
+    RmiProxyFactoryBean userService() {
+        RmiProxyFactoryBean rmiProxyFactory = new RmiProxyFactoryBean();
+        rmiProxyFactory.setServiceUrl("rmi://localhost:1099/UserServiceI");
+        rmiProxyFactory.setServiceInterface(UserServiceI.class);
+        return rmiProxyFactory;
+    }
+
+    @Bean
+    RmiProxyFactoryBean setService() {
+        RmiProxyFactoryBean rmiProxyFactory = new RmiProxyFactoryBean();
+        rmiProxyFactory.setServiceUrl("rmi://localhost:1099/SetServiceI");
+        rmiProxyFactory.setServiceInterface(SetServiceI.class);
+        return rmiProxyFactory;
+    }
+
+    @Bean
+    RmiProxyFactoryBean artefactService() {
+        RmiProxyFactoryBean rmiProxyFactory = new RmiProxyFactoryBean();
+        rmiProxyFactory.setServiceUrl("rmi://localhost:1099/ArtefactServiceI");
+        rmiProxyFactory.setServiceInterface(ArtefactServiceI.class);
+        return rmiProxyFactory;
+    }
 
 	public static void main(String[] args) {
 	    try{
@@ -30,7 +57,7 @@ public class ClientApplication {
             user    = new User("Denis","1234","e@mail.co",0,0);
             service.register(user);
             System.out.println("Reg succesfull");
-            service.auth(user);
+            //service.auth(user);
             System.out.println("I`m authed");
         } catch (UserException e) {
             e.printStackTrace();
---
+++ src/main/java/com/cw/models/db/services/UserServiceI.java
@@ -0,0 +1,26 @@
+package com.cw.models.db.services;
+
+import com.cw.models.entities.User;
+
+/**
+ * Created by denysmelnychenko on 2/11/18.
+ */
+public interface UserServiceI {
+    User getUserById(int id);
+    User getUserByUsername(String username);
+    User getUserByEmail(String email);
+    User getUserByEmailAndPassword(String email, String password);
+
+    boolean addUser(User user);
+    boolean updateUser(User user);
+
+    boolean deleteUserById(int id);
+    User deleteUser(User user);
+
+    void closeConnection();
+
+    //TODO this method. Add "throws AuthentificationException"
+    void authentificate(User user);
+    //TODO this method. Add "throws AuthentificationException"
+    void register(User user);
+}
